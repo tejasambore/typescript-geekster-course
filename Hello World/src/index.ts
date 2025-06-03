e4d7c72916af322e9777e59b9ccc4a8b766a814f
Tejas <tejasambore2003@gmail.com>
2025-06-03 16:24:27 +0530

    Getting started with TypeScript

---
+++ Hello World/src/index.ts
@@ -0,0 +1,10 @@
+// console.log("Hello World..!");
+
+// let courseName:string = "TypeScript";
+// let numbers:number = 12345567;
+// let isAdmin:boolean = true;
+// let value;
+// value = 1;
+
+// Array:
+let arr:number[] = [1, 3, 5, 6, 7, 9];
